48fd16f56c5091cfd16ec7db5958c44d1bc0224d
rjmanzo <manzorenato@gmail.com>
2020-03-03 13:09:38 -0300

    Rebasing changes

---
+++ jsx/src/index.js
@@ -0,0 +1,28 @@
+// Import libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function getButtonText(){
+    return 'click on Me!';
+}
+
+//create a react component
+const App = () => {
+    const buttonText = {text: 'Click Me'}; 
+
+    return (
+        <div>
+            <label className="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+    <button style={{backgroundColor: 'blue',color: 'white'}}> {buttonText.text}</button>
+
+        </div>
+    );
+};
+
+//Take the react component and show it on the screen
+ReactDOM.render( 
+    <App /> ,
+    document.querySelector('#root'));
